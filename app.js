d4bf41f44444e3c791f99f2f0da413208eb7c5ef
cramerchristian <christian_cramer@me.com>
2020-08-25 18:02:36 +0200

    game finished, to add: more functionality

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
-// init game
-// - create Divs -> Array
-// - create Snake -> Array
-// div classes of snake & veggies
-// create Movement of Snake -> lastInput Variable
-// check if Snake hits Snake
-// create new Veggies
-// check if Veggies are Eaten
+// to fix: edge case with veggie eating
+// to add: scoring and reset Button
+// to add: different type of veggies / one that slows down, one that speeds up
 
 const $grid = document.getElementById('grid');
 const snakeArr = [175, 176, 177, 178, 179, 199, 219, 239];
 const gridArr = [];
-const veggie = 72;
-let lastInput = 'UP';
+let lastInput = 'LEFT';
 let direction = -1;
 
 function initGame() {
+  // draw divs
   for (let i = 0; i < 400; i++) {
     const $field = document.createElement('div');
     $grid.appendChild($field);
     gridArr.push($field);
   }
+
+  // draw initial snake
   snakeArr.forEach((snake) => gridArr[snake].classList.add('snake'));
+
+  // draw first veggie
+  renderVeggie();
+
+  // init cursor eventlistener
+  document.addEventListener('keyup', function (event) {
+    lastInput = setLastInput(event);
+  });
 }
 
+const gameLoop = setInterval(() => {
+  renderSnake();
+}, 150);
+
 function renderSnake() {
+  getNewDirection();
+  checkforVeggie();
+  checkGameOver();
   snakeArr.forEach((snake) => gridArr[snake].classList.remove('snake'));
   moveSnake();
   snakeArr.forEach((snake) => gridArr[snake].classList.add('snake'));
+}
+
+function getNewDirection() {
+  switch (lastInput) {
+    case 'LEFT':
+      direction = -1;
+      break;
+    case 'RIGHT':
+      direction = 1;
+      break;
+    case 'UP':
+      direction = -20;
+      break;
+    case 'DOWN':
+      direction = 20;
+      break;
+  }
 }
 
 function setLastInput(event) {
@@ -44,45 +73,40 @@ function setLastInput(event) {
   }
 }
 
-function moveSnake() {
-  switch (lastInput) {
-    case 'LEFT':
-      direction = -1;
-      break;
-    case 'RIGHT':
-      direction = 1;
-      break;
-    case 'UP':
-      direction = -20;
-      break;
-    case 'DOWN':
-      direction = 20;
-      break;
-  }
-
-  checkGameOver();
-
-  snakeArr.unshift(snakeArr[0] + direction);
-  snakeArr.pop(snakeArr[snakeArr.length - 1]);
-}
-
-const gameLoop = setInterval(() => {
-  renderSnake();
-}, 500);
-
 function checkGameOver() {
   if (
     (lastInput === 'LEFT' && snakeArr[0] % 20 === 0) ||
     (lastInput === 'RIGHT' && (snakeArr[0] + direction) % 20 === 0) ||
     (lastInput === 'UP' && snakeArr[0] + direction < 0) ||
-    (lastInput === 'DOWN' && snakeArr[0] + direction > 399)
+    (lastInput === 'DOWN' && snakeArr[0] + direction > 399) ||
+    gridArr[snakeArr[0] + direction].classList.contains('snake')
   ) {
     clearInterval(gameLoop);
     alert('GameOver');
   }
 }
 
+function moveSnake() {
+  snakeArr.unshift(snakeArr[0] + direction);
+  snakeArr.pop(snakeArr[snakeArr.length - 1]);
+}
+
+function getNewVeggie() {
+  const veggie = Math.floor(Math.random() * 400);
+  return gridArr[veggie].classList.contains('snake') ? getNewVeggie() : veggie;
+}
+
+function renderVeggie() {
+  gridArr[getNewVeggie()].classList.add('veggie');
+}
+
+function checkforVeggie() {
+  if (gridArr[snakeArr[0] + direction].classList.contains('veggie')) {
+    gridArr[snakeArr[0] + direction].classList.remove('veggie');
+    gridArr[snakeArr[0] + direction].classList.add('snake');
+    snakeArr.unshift(snakeArr[0] + direction);
+    renderVeggie();
+  }
+}
+
 initGame();
-document.addEventListener('keyup', function (event) {
-  lastInput = setLastInput(event);
-});
